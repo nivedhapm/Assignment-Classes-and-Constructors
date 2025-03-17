246350921a2a9c56ba98e6afcf4ba3783b2161b3
NIVEDHA P M <121153649+nivedhapm@users.noreply.github.com>
2025-03-17 14:21:47 +0530

    TeamPlayerDemo.java

--- classesAndObjects/TeamPlayerDemo.java
+++ classesAndObjects/TeamPlayerDemo.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class TeamPlayerDemo {
     public static void main(String[] args) {
-        Player player1 = new Player("John Doe", "Forward", 10);
-        Player player2 = new Player("Alice Smith", "Goalkeeper", 1);
+        Player player1 = new Player("Messi", "Forward", 10);
+        Player player2 = new Player("Ronaldo", "Forward", 7);
 
         List<Player> teamPlayers = new ArrayList<>();
         teamPlayers.add(player1);
         teamPlayers.add(player2);
 
-        Team team = new Team("Tigers", "New York", "Division A", teamPlayers);
+        Team team = new Team("Portugal national team", "Portuguese", "Division A", teamPlayers);
 
         System.out.println(team);
     }
